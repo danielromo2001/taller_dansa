fba7886a37644902125f79d402433ab98d514f04
Daniel Rodriguez <danielfrodriguez@unicesar.edu.co>
2023-06-09 14:09:01 -0500

    Foto del mecanico

--- lib/pages/registrar_mecanico/registrar_mecanico_widget.dart
+++ lib/pages/registrar_mecanico/registrar_mecanico_widget.dart
@@ -1,14 +1,17 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
+import '/backend/firebase_storage/storage.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/upload_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
 import 'registrar_mecanico_model.dart';
 export 'registrar_mecanico_model.dart';
 
@@ -54,13 +57,13 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
           automaticallyImplyLeading: false,
           leading: FlutterFlowIconButton(
             borderColor: Colors.transparent,
-            borderRadius: 30.0,
-            borderWidth: 1.0,
-            buttonSize: 60.0,
+            borderRadius: 30,
+            borderWidth: 1,
+            buttonSize: 60,
             icon: Icon(
               Icons.arrow_back_rounded,
               color: Colors.white,
-              size: 30.0,
+              size: 30,
             ),
             onPressed: () async {
               context.pop();
@@ -71,12 +74,12 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Poppins',
                   color: Colors.white,
-                  fontSize: 22.0,
+                  fontSize: 22,
                 ),
           ),
           actions: [],
           centerTitle: true,
-          elevation: 2.0,
+          elevation: 2,
         ),
         body: SafeArea(
           top: true,
@@ -85,7 +88,7 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 20.0, 0.0, 0.0),
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
                   child: Container(
                     width: MediaQuery.of(context).size.width * 0.9,
                     height: MediaQuery.of(context).size.width * 0.9,
@@ -95,8 +98,8 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                         fit: BoxFit.cover,
                         image: Image.asset(
                           Theme.of(context).brightness == Brightness.dark
-                              ? 'assets/images/1.png'
-                              : 'assets/images/2.png',
+                              ? 'assets/images/mcatd_1.png'
+                              : 'assets/images/9hsjc_2.png',
                         ).image,
                       ),
                       shape: BoxShape.circle,
@@ -104,21 +107,19 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                   ),
                 ),
                 Container(
-                  width: MediaQuery.of(context).size.width * 1.0,
-                  height: 480.0,
+                  width: MediaQuery.of(context).size.width,
+                  height: 480,
                   decoration: BoxDecoration(
                     color: FlutterFlowTheme.of(context).secondaryBackground,
                   ),
                   child: Padding(
-                    padding:
-                        EdgeInsetsDirectional.fromSTEB(0.0, 30.0, 0.0, 0.0),
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                     child: Column(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              8.0, 10.0, 8.0, 0.0),
+                          padding: EdgeInsetsDirectional.fromSTEB(8, 5, 8, 0),
                           child: TextFormField(
                             controller: _model.nameMecanicoController,
                             autofocus: true,
@@ -129,7 +130,7 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                   .labelMedium
                                   .override(
                                     fontFamily: 'Poppins',
-                                    fontSize: 18.0,
+                                    fontSize: 18,
                                     fontWeight: FontWeight.w600,
                                   ),
                               hintText: 'Digite el nombre del mecanico',
@@ -137,37 +138,37 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                   .labelMedium
                                   .override(
                                     fontFamily: 'Poppins',
-                                    fontSize: 15.0,
+                                    fontSize: 15,
                                     fontWeight: FontWeight.w200,
                                     fontStyle: FontStyle.italic,
                                   ),
                               enabledBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: Color(0xB32FB73C),
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               focusedBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: Color(0xB32FB73C),
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               errorBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: FlutterFlowTheme.of(context).error,
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               focusedErrorBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: FlutterFlowTheme.of(context).error,
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                             ),
                             style: FlutterFlowTheme.of(context).bodyMedium,
@@ -176,8 +177,7 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                           ),
                         ),
                         Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              8.0, 20.0, 8.0, 0.0),
+                          padding: EdgeInsetsDirectional.fromSTEB(8, 20, 8, 0),
                           child: TextFormField(
                             controller: _model.phoneController,
                             autofocus: true,
@@ -188,7 +188,7 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                   .labelMedium
                                   .override(
                                     fontFamily: 'Poppins',
-                                    fontSize: 18.0,
+                                    fontSize: 18,
                                     fontWeight: FontWeight.w600,
                                   ),
                               hintText: 'Digite el telefono',
@@ -196,37 +196,37 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                   .labelMedium
                                   .override(
                                     fontFamily: 'Poppins',
-                                    fontSize: 15.0,
+                                    fontSize: 15,
                                     fontWeight: FontWeight.w200,
                                     fontStyle: FontStyle.italic,
                                   ),
                               enabledBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: Color(0xB32FB73C),
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               focusedBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: Color(0xB32FB73C),
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               errorBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: FlutterFlowTheme.of(context).error,
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                               focusedErrorBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
                                   color: FlutterFlowTheme.of(context).error,
-                                  width: 0.0,
+                                  width: 0,
                                 ),
-                                borderRadius: BorderRadius.circular(20.0),
+                                borderRadius: BorderRadius.circular(20),
                               ),
                             ),
                             style: FlutterFlowTheme.of(context).bodyMedium,
@@ -234,29 +234,112 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                 .asValidator(context),
                           ),
                         ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.width * 0.4,
+                          decoration: BoxDecoration(
+                            color: FlutterFlowTheme.of(context)
+                                .secondaryBackground,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                            child: InkWell(
+                              splashColor: Colors.transparent,
+                              focusColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              onTap: () async {
+                                final selectedMedia = await selectMedia(
+                                  mediaSource: MediaSource.photoGallery,
+                                  multiImage: false,
+                                );
+                                if (selectedMedia != null &&
+                                    selectedMedia.every((m) =>
+                                        validateFileFormat(
+                                            m.storagePath, context))) {
+                                  setState(() => _model.isDataUploading = true);
+                                  var selectedUploadedFiles =
+                                      <FFUploadedFile>[];
+                                  var downloadUrls = <String>[];
+                                  try {
+                                    selectedUploadedFiles = selectedMedia
+                                        .map((m) => FFUploadedFile(
+                                              name:
+                                                  m.storagePath.split('/').last,
+                                              bytes: m.bytes,
+                                              height: m.dimensions?.height,
+                                              width: m.dimensions?.width,
+                                              blurHash: m.blurHash,
+                                            ))
+                                        .toList();
+
+                                    downloadUrls = (await Future.wait(
+                                      selectedMedia.map(
+                                        (m) async => await uploadData(
+                                            m.storagePath, m.bytes),
+                                      ),
+                                    ))
+                                        .where((u) => u != null)
+                                        .map((u) => u!)
+                                        .toList();
+                                  } finally {
+                                    _model.isDataUploading = false;
+                                  }
+                                  if (selectedUploadedFiles.length ==
+                                          selectedMedia.length &&
+                                      downloadUrls.length ==
+                                          selectedMedia.length) {
+                                    setState(() {
+                                      _model.uploadedLocalFile =
+                                          selectedUploadedFiles.first;
+                                      _model.uploadedFileUrl =
+                                          downloadUrls.first;
+                                    });
+                                  } else {
+                                    setState(() {});
+                                    return;
+                                  }
+                                }
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Image.network(
+                                  'https://picsum.photos/seed/829/600',
+                                  width: 300,
+                                  height: 200,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Cargar imagen del mecanico',
+                          style: FlutterFlowTheme.of(context).bodyMedium,
+                        ),
                         Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              0.0, 30.0, 0.0, 0.0),
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                           child: FFButtonWidget(
                             onPressed: () async {
                               final mecanicosCreateData =
                                   createMecanicosRecordData(
                                 nombre: _model.nameMecanicoController.text,
                                 telefono: _model.phoneController.text,
+                                foto: _model.uploadedFileUrl,
                               );
                               await MecanicosRecord.collection
                                   .doc()
                                   .set(mecanicosCreateData);
-
-                              context.pushNamed('Home');
                             },
                             text: 'Guardar',
                             options: FFButtonOptions(
-                              height: 40.0,
-                              padding: EdgeInsetsDirectional.fromSTEB(
-                                  24.0, 0.0, 24.0, 0.0),
-                              iconPadding: EdgeInsetsDirectional.fromSTEB(
-                                  0.0, 0.0, 0.0, 0.0),
+                              height: 40,
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                              iconPadding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
                               color: Color(0xFF2FCA6B),
                               textStyle: FlutterFlowTheme.of(context)
                                   .titleSmall
@@ -264,12 +347,12 @@ class _RegistrarMecanicoWidgetState extends State<RegistrarMecanicoWidget> {
                                     fontFamily: 'Poppins',
                                     color: Colors.white,
                                   ),
-                              elevation: 3.0,
+                              elevation: 3,
                               borderSide: BorderSide(
                                 color: Colors.transparent,
-                                width: 1.0,
+                                width: 1,
                               ),
-                              borderRadius: BorderRadius.circular(8.0),
+                              borderRadius: BorderRadius.circular(8),
                             ),
                           ),
                         ),
